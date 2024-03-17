53ea9df8c6f7bf8a7b3dfba4f38108d997b8d5aa
Hugo Fontaine <hugo.fontaine@protonmail.com>
2024-03-17 17:50:22 +0100

    .text in FLASH

--- init.c
+++ init.c
@@ -13,15 +13,7 @@ void exit(void) {
 }
 
 __attribute__((section(".bootloader"))) void init() {
-    //init .text, must be initialized first to be able to use memfuncs
-    uint8_t* destination = (uint8_t*)&_text_start_VMA;
-    uint8_t* source = (uint8_t*)&_text_start_LMA;
-    for (size_t i = 0; i < (size_t) &_text_size; i++)
-    {
-        *(destination++) = *(source++);
-    }
-    //memcpy(&_vector_start_VMA, &_vector_start_LMA, (size_t) &_vector_size); //init .vector_table
-    memcpy(&_data_start_VMA, &_data_start_LMA, (size_t) &_data_size); //init .data*/
+    memcpy(&_data_start_VMA, &_data_start_LMA, (size_t) &_data_size); //init .data
     memset(&_bss_start_VMA, 0, (size_t) &_bss_size); //init .bss
 
     main();
